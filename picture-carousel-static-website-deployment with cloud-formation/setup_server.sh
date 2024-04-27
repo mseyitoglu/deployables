f7d429a1e3c150e1888e4d50d5ba64b3a2310e08
mseyitoglu <mseyitoglu@outlook.com>
2024-04-27 16:55:40 -0500

    corrected the prohect title

--- picture-carousel-static-website-deployment with cloud-formation/setup_server.sh
+++ picture-carousel-static-website-deployment with cloud-formation/setup_server.sh
@@ -1,6 +1,12 @@
-#!
+#!/bin/bash -x
+
+# update the system and install apache
+
 yum update -y
 yum install httpd -y
+
+#change to the current directory and download web content
+
 cd /var/www/html
 FOLDER="https://raw.githubusercontent.com/mseyitoglu/deployables/main/picture-carousel-static-website-deployment%20with%20cloud-formation/static-web/"
 wget ${FOLDER}/index.html
--- simple web-app roman-numerals-converter/setup.sh
+++ simple web-app roman-numerals-converter/setup.sh
@@ -2,15 +2,18 @@
 
 yum update -y
 yum install python3 -y
-yum install pip3 -y
-pip3 install flask
+yum install pip -y
 pip install flask
 yum update -y
 
 APP="https://raw.githubusercontent.com/mseyitoglu/deployables/main/simple%20web-app%20roman-numerals-converter"
 cd /home/ec2-user/
 wget ${APP}/app.py
-mkdir app && cd app
+mkdir templates && cd templates
 wget ${APP}/templates/index.html
 wget ${APP}/templates/result.html
+cd ..
+python3 app.py
+
+
 
