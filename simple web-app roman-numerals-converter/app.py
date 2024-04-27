3325658ff57d47bade8560646a2ba21e49d065e4
mseyitoglu <mseyitoglu@outlook.com>
2024-04-27 15:05:23 -0500

    changed the port

--- simple web-app roman-numerals-converter/app.py
+++ simple web-app roman-numerals-converter/app.py
@@ -26,6 +26,6 @@ def main_post():
         return render_template('index.html', developer_name='Zach', not_valid=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
     
-    #app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
--- simple web-app roman-numerals-converter/setup.sh
+++ simple web-app roman-numerals-converter/setup.sh
@@ -6,10 +6,11 @@ yum install pip3 -y
 pip3 install flask
 pip install flask
 yum update -y
-mkdir app
-cd /home/ec2-user/app
+
 APP="https://raw.githubusercontent.com/mseyitoglu/deployables/main/simple%20web-app%20roman-numerals-converter"
+cd /home/ec2-user/
 wget ${APP}/app.py
+mkdir app && cd app
 wget ${APP}/templates/index.html
 wget ${APP}/templates/result.html
 
